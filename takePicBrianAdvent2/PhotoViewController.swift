a73bbcc5e9b873153266402d22eefaac3d62ec17
amqzong <amqzong@gmail.com>
2018-08-21 15:30:07 -0400

    Update PhotoViewController.swift

--- takePicBrianAdvent2/PhotoViewController.swift
+++ takePicBrianAdvent2/PhotoViewController.swift
@@ -1,4 +1,5 @@
 //  PhotoViewController.swift
+//  Description: Allows users to view their taken photo and retake if needed. Assigns the image a unique identifying tag, uploads the image to Dropbox, which connects to a remote melanoma classification model that extracts melanoma indicative features from the image, applies learned parameters onto the features, and outputs a diagnosis and related figures to Dropbox with the same identifying tag as the original image. The app then receives the results and passes it to FigureViewController.
 //  Created by Amanda Zong on 7/18/17.
 //  Credits: Camera App Swift Tutorial by Brian Advent (https://www.youtube.com/watch?v=Zv4cJf5qdu0)
 
